a1925f27ecb604fc578982badefc99d617afc88a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-01-06 17:15:18 -0300

    Parte 13 da aula 1 do curso C I: Introdução à Linguagem das Linguagens

--- olaLinguagemC.c
+++ olaLinguagemC.c
@@ -1,4 +1,4 @@
-//Escreva um programa que escreva Ola linguagem C na tela. Para isso, crie um arquivo .c, escreva a função main com um printf dentro (igual no vídeo). Depois, use o GCC para compilar seu programa.
+// Escreva um programa que escreva Ola linguagem C na tela. Para isso, crie um arquivo .c, escreva a função main com um printf dentro (igual no vídeo). Depois, use o GCC para compilar seu programa.
 
 #include <stdio.h>
 
