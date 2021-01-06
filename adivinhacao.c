d89ff7683ab3054b88ebcb9a20f2376f4cacc93e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-01-06 16:42:50 -0300

    Parte 7 da aula 1 do curso C I: Introdução à Linguagem das Linguagens

--- adivinhacao.c
+++ adivinhacao.c
@@ -6,4 +6,6 @@ int main()
 	printf("**************************************\n");
 	printf("Bem vindo ao nosso jogo de adivinhação\n");
 	printf("**************************************\n");
+
+	int numeroSecreto = 42;
 }
