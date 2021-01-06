0cab08645f4df8c0e8a095ee2a9b90c1278fdc81
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-01-06 16:35:14 -0300

    Parte 6 da aula 1 do curso C I: Introdução à Linguagem das Linguagens

---
+++ adivinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	// Imprime o cabeçalho do nosso jogo
+	printf("**************************************\n");
+	printf("Bem vindo ao nosso jogo de adivinhação\n");
+	printf("**************************************\n");
+}
