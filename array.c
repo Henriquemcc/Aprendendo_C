f509c44526ed64d6b8577f26aea9e58a0a4d3a8f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-12-21 19:13:17 -0300

    Parte 2 da aula 2 do curso C II: Avançando na linguagem

--- array.c
+++ array.c
@@ -28,8 +28,76 @@ int notas() {
 
 }
 
+int melancia1() {
+
+    char palavra[9];
+
+    palavra[0] = 'M';
+    palavra[1] = 'E';
+    palavra[2] = 'L';
+    palavra[3] = 'A';
+    palavra[4] = 'N';
+    palavra[5] = 'C';
+    palavra[6] = 'I';
+    palavra[7] = 'A';
+
+    printf("%c%c%c%c%c%c%c%c\n", palavra[0], palavra[1], palavra[2], palavra[3], palavra[4], palavra[5], palavra[6], palavra[7]);
+
+}
+
+int melancia2() {
+
+    char palavra[9];
+
+    sprintf(palavra, "MELANCIA");
+
+    printf("%s\n", palavra);
+
+}
+
+int melancia3() {
+
+    char palavra[9];
+
+    palavra[0] = 'M';
+    palavra[1] = 'E';
+    palavra[2] = 'L';
+    palavra[3] = 'A';
+    palavra[4] = 'N';
+    palavra[5] = 'C';
+    palavra[6] = 'I';
+    palavra[7] = 'A';
+    palavra[8] = '\0';
+
+    printf("%s\n", palavra);
+
+}
+
+int paralelepipedo() {
+
+    char palavra[15];
+
+    sprintf(palavra, "PARALELEPIPEDO");
+
+    printf("%s\n", palavra);
+}
+
+int pneumoultramicroscopicossilicovulcanoconiotico() {
+    
+    char palavra[47];
+
+    sprintf(palavra, "pneumoultramicroscopicossilicovulcanoconiotico");
+
+    printf("%s\n", palavra);
+}
+
 int main() {
 
     notas();
+    melancia1();
+    melancia2();
+    melancia3();
+    paralelepipedo();
+    pneumoultramicroscopicossilicovulcanoconiotico();
 
 }
